fe92495a29755d8a4e31836dc3db40b2cd2eb753
unknown <valis.source@gmail.com>
2018-12-08 05:21:21 +0200

    Добавил поведение по юзеру  в базе

--- app_api/controllers/parser.js
+++ app_api/controllers/parser.js
@@ -1,28 +1,54 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('users');
 
-//Хелпер для отправки ответа в JSON
+//Отправка ответа в JSON
 var sendJSONresponse = function(res, status, content) {
     res.status(status);
     res.json(content);
   };
 
+//Отправка ответа в тексте
+var sendCsvResponse=function(res,status,content){
+    console.log('stub');
+}
 
 var getData=function(req,res){
-    let youtubeId=req.query.socialblade;
+    let socialbladeID=req.query.socialblade;
     let patreon=req.query.patreon;
-    let userTime=req.query.gmt;
+    let userTime=req.query.gmt ? req.query.gmt:0;
+    let sendResponse=sendJSONresponse;
 
-    if (!youtubeId){
-        sendJSONresponse(res,400,{'error':'youtube ID is not passed'});
+    if (!socialbladeID){
+        sendResponse(res,400,{'error':'socialbladeID ID is not passed'});
         return;
     }
     if (!patreon){
-        sendJSONresponse(res,400,{'error':'patreon ID is not passed'});
+        sendResponse(res,400,{'error':'patreon ID is not passed'});
         return;
     }
 
-    sendJSONresponse(res,200,{'ok':'Sucscess!!!!'});
+    User.findOneOrCreate({
+        'socialbladeID':socialbladeID,
+        'timezone':userTime},
+        function (err,user){
+            if (err){
+                sendResponse(res,523,'db Error');
+            }
+            else {
+                //Пообновляем данные по юзеру
+                user.timezone=userTime;
+                user.lastRequest=Date.now();
+                user.save((err)=>console.log(err));
+
+                //Здесь пошел парсинг
+
+            
+            }
+        }
+        );
+
+
+        sendResponse(res,200,{'ok':'Sucscess!!!!'});
 }
 
 module.exports.getData=getData;
