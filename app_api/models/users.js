07a9cc9eb7e3997b2ab1b509d38f69fcc8b354ab
valis1 <valis.surce@gmail.com>
2018-12-10 10:16:06 +0000

    Исправил баг - True на true

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -17,7 +17,7 @@ var userSchema=new mongoose.Schema(
 userSchema.statics.findOneOrCreate = function findOneOrCreate(condition, callback) {
     const self = this;
     self.findOne({'socialbladeID':condition.socialbladeID,'patreonLogin':condition.patreonLogin}, (err, result) => {
-        return result ? callback(err, result) : self.create(condition, (err, result) => { result.created=True; return callback(err, result); });
+        return result ? callback(err, result) : self.create(condition, (err, result) => { result.created=true; return callback(err, result); });
     });
 };
 
