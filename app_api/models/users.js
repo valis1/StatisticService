29f3a7a4eddb4b74800a6805727efb7f37a28762
valis1 <valis.surce@gmail.com>
2018-12-10 10:03:18 +0000

    Фиксы

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -15,10 +15,10 @@ var userSchema=new mongoose.Schema(
 );
 
 userSchema.statics.findOneOrCreate = function findOneOrCreate(condition, callback) {
-    const self = this
+    const self = this;
     self.findOne({'socialbladeID':condition.socialbladeID,'patreonLogin':condition.patreonLogin}, (err, result) => {
-        return result ? callback(err, result) : self.create(condition, (err, result) => { return callback(err, result) })
-    })
-}
+        return result ? callback(err, result) : self.create(condition, (err, result) => { result.created=True; return callback(err, result); });
+    });
+};
 
 mongoose.model('users', userSchema);
